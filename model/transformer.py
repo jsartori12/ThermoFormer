3e1a061d1950ef767ee904702a74f88622e911ce
João Sartori <53714290+jsartori12@users.noreply.github.com>
2026-04-23 11:03:56 -0300

    Add files via upload

--- model/transformer.py
+++ model/transformer.py
@@ -64,8 +64,42 @@ class ThermoTranslator(nn.Module):
         return logits
 
     @torch.no_grad()
-    def generate(self, src_ids, max_new_tokens=512, temperature=1.0):
-        pass
+    def generate(self, src_ids, max_new_tokens=512, temperature=1.0, top_k = 50):
+        self.eval()
+        device = src_ids.device
+    
+        
+        src_mask = self._pad_mask(src_ids, self.cfg.pad_token_id)
+        enc_out  = self.encode(src_ids, src_mask)
+    
+        
+        generated = torch.tensor([[self.cfg.bos_token_id]],
+                                  dtype=torch.long, device=device)
+    
+        for _ in range(max_new_tokens):
+            tgt_mask = self._causal_mask(generated.size(1), device)
+            logits   = self.decode(generated, enc_out,
+                                   tgt_mask=tgt_mask,
+                                   src_mask=src_mask)
+    
+            # get only last generated token
+            next_logits = logits[:, -1, :] / temperature
+            
+            #### Implementar opcao para usar topK no lugar de argmax
+            #next_token  = next_logits.argmax(dim=-1, keepdim=True)
+            if top_k > 0:
+                values, _ = torch.topk(next_logits, top_k)
+                threshold  = values[:, -1].unsqueeze(-1)
+                next_logits = next_logits.masked_fill(next_logits < threshold, float("-inf"))
+            probs      = torch.softmax(next_logits, dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1)
+
+            generated = torch.cat([generated, next_token], dim=1)
+    
+            if next_token.item() == self.cfg.eos_token_id:
+                break
+    
+        return generated[0].tolist()
     
     
 # cfg   = ModelConfig()
